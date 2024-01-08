e3414664f31137e86b6c45e0c9801c0c08e6b646
Татьяна Андреева <tan-andreeva@mail.ru>
2024-01-08 14:41:37 +0400

    a

---
+++ bin/gendiff.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander';
+
+
+const program = new Command();
+
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1')
+  .option('-V, --version', 'output the version number')
+  .option('-h, --help', 'output usage information');
+program.parse();
