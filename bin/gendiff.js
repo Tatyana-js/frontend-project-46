9d4953333e4abac2009f872e93f6abb18eb74f2d
Татьяна Андреева <tan-andreeva@mail.ru>
2024-01-11 11:39:10 +0400

    add  bin/gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,9 +7,9 @@ const program = new Command();
 
 program
   .name('gendiff')
+  .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
-  .option('-V, --version', 'output the version number')
-  .option('-h, --help', 'output usage information')
-  .parse();
-
-
+  .option('-f, --format <type>', 'output format', 'stylish')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .parse()
