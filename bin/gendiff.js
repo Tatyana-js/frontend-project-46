d5a39e75ea89fc8b27de44c1d25099db7ace5d98
Татьяна Андреева <tan-andreeva@mail.ru>
2024-01-15 18:26:52 +0300

    add Makefile bin/gendiff.js  src/index.js src/parse.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -9,8 +9,10 @@ program
   .name('gendiff')
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
-  // .option('-f, --format <type>', 'output format', 'stylish')
-  .arguments('<filepath1>', '<filepath2>')
-  .action(gendiff)
-  .parse(process.argv);
-gendiff();
+  .option('-f, --format <type>', 'output format')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .action((filepath1, filepath2) => {
+    console.log(gendiff(filepath1, filepath2, program.opts().format));
+  })
+  .parse();
--- src/parse.js
+++ src/parse.js
@@ -1,3 +1,18 @@
-import fs from 'fs';
+import fs from 'node:fs';
+import process from 'node:process';
+import path from 'node:path';
 
-const parse
+export const getFilePath = (file) => path.resolve(process.cwd(), '../__fixtures__', file);
+
+getFilePath();
+const parsing = (file) => {
+  const content = fs.readFileSync(getFilePath(file), 'utf8');
+  const format = path.extname(getFilePath(file));
+  switch (format) {
+    case '.json':
+      return JSON.parse(content);
+    default:
+      throw new Error('Unknown format!');
+  }
+};
+export default parsing;
