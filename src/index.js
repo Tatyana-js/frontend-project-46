652057605d46dd29cbc308ee504eb407ba800252
Татьяна Андреева <tan-andreeva@mail.ru>
2024-01-16 19:52:55 +0300

    add index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import fs from 'node:fs';
 import process from 'node:process';
 import path from 'node:path';
 import _ from 'lodash';
+import parse from './parse.js';
 
 export const getFilePath = (file) => {
   const filePath = path.resolve(process.cwd(), './__fixtures__', file);
@@ -13,29 +14,30 @@ export const getFileContent = (filePath) => {
   return dataFale;
 };
 
-const getFileType = (filePath) => {
+const getFormat = (filePath) => {
   const format = path.extname(filePath).slice(1);
   return format;
 };
 
 const gendiff = (filepath1, filepath2) => {
-    const resultStr = '';
-    const data1 = parse(getFilePath(filepath1), getFileContent(filepath1));
-    const data2 = parse(getFilePath(filepath2), getFileContent(filepath2));
-    const arr1 = Object.keys(data1);
-    const arr2 = Object.keys(data2);
-        for (const item of arr1) {
-          if (_.has(obj2, item)) {
-            resultObj[item] = obj1[item] === obj2[item] ? 'unchanged' : 'changed';
-          } else if (!_.has(obj2, item)) {
-            resultObj[item] = 'deleted';
-          }
-        } for (const item of arr2) {
-          if (!_.has(obj1, item)) {
-            resultObj[item] = 'added'; 
-          }
-        }
-        return resultObj;
-      };
+  const data1 = parse(getFileContent(filepath1), getFormat(filepath1));
+  const data2 = parse(getFileContent(filepath2), getFormat(filepath2));
+  const value1 = data1.key;
+  const value2 = data2.key;
+  const commonKeys = Object.keys({ ...data1, ...data2 }).sort();
+  const result = commonKeys.map((key) => {
+    if (_.has(data1, key) && _.has(data2, key) && !value1 === value2) {
+      return `- ${key}: ${value1}\n + ${key}: ${value2}`;
+    }
+    if (_.has(data1, key) && _.has(data2, key) && value1 === value2) {
+      return `${key}: ${value1}`;
+    }
+    if (_.has(data1, key) && !_.has(data2, key)) {
+      return `- ${key}: ${value1}`;
+    }
+    return `+ ${key}: ${value1}`;
+  });
+  return result.join('\n');
+};
 
 export default gendiff;
